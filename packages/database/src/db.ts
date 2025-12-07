307bb75996ef0bc2670494d92c35110f6b72e768
assim habeek <assimhabe@gmail.com>
2025-12-07 11:36:13 +0100

    upgrade: turbo 2.6.2

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { planetTable } from "./schema";
 
+// biome-ignore lint/style/noNonNullAssertion: will be replaced very soon
 export const db = drizzle(process.env.DATABASE_URL!, {
 	schema: { planets: planetTable },
 });
